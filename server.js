5e8ebcea7939c88f137ce12c409bbd3d52e354bb
Alan Fleming <alalfleming@gmail.com>
2018-03-10 20:42:22 -0500

    bundle.js connected

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 9000;
+app.listen( port, ()=>console.log(`Listening on port ${port}`));
